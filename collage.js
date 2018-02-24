7bb8e0a790f0716dd6e8b165c4a718efe4a99e61
Vinay Rajagopal <vprajago@usc.edu>
2018-02-24 15:39:04 -0800

    made request to Servlet and creating collage objs

--- collage.js
+++ collage.js
@@ -1,11 +1,45 @@
 // JavaScript Document
-$("button").click(function(){
-	$("body").removeClass("default");
-	$("body").addClass("activated");
-	setTimeout(function(){
-		$("#collage").css("left", ($(this).width()- $("#collage").width())/2);
-	}, 550);
+var collages = [];
+var url = "http::localhost:8080/CollageServlet/";
+
+$(document).ready(function() {
+  $("button").click(function(){
+    handleButtonClick();
+  });
+  positionCollage();
 });
-$( window ).resize(function() {
-	$("#collage").css("left", ($(this).width()- $("#collage").width())/2);
-});
+
+
+function positionCollage() {
+  $( window ).resize(function() {
+  	$("#collage").css("left", ($(this).width()- $("#collage").width())/2);
+  });
+}
+
+function handleButtonClick() {
+  var searchQuery = $("#input-field").val();
+  if(searchQuery.length > 0 && searchQuery != undefined) {
+    var collageObj = makeRequest(searchQuery);
+    collages.push(collageObj);
+
+    $("body").removeClass("default");
+    $("body").addClass("activated");
+    setTimeout(function(){
+      $("#collage").css("left", ($(this).width()- $("#collage").width())/2);
+    }, 550);
+  }
+}
+
+function makeRequest(queryString) { // Make request to Servelet
+  $.get(url + "?query=" + queryString)
+  .done(function(data) {
+    return {
+      src: data.src,
+      name: data.query,
+      isActive: false, // if it will show up in the timeline
+    };
+  })
+  .fail(function() {
+    console.log("Request to " + url + "?query=" + queryString + " failed");
+  });
+}
