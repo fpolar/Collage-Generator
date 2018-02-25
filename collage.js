c605cc59751f120d5bd96af38971308a6114d1af
Vinay Rajagopal <vprajago@usc.edu>
2018-02-24 16:04:32 -0800

    VR AJAX Impl (#1)
    * made request to Servlet and creating collage objs
    
    * serialized multi word queries
    
    * refactored collageObj

--- collage.js
+++ collage.js
@@ -1,11 +1,46 @@
 // JavaScript Document
-$("button").click(function(){
-	$("body").removeClass("default");
-	$("body").addClass("activated");
-	setTimeout(function(){
-		$("#collage").css("left", ($(this).width()- $("#collage").width())/2);
-	}, 550);
+var collages = [];
+var url = "http::localhost:8080/CollageServlet/?";
+
+$(document).ready(function() {
+  $("button").click(function(){
+    handleButtonClick();
+  });
+  positionCollage();
 });
-$( window ).resize(function() {
-	$("#collage").css("left", ($(this).width()- $("#collage").width())/2);
-});
+
+
+function positionCollage() {
+  $( window ).resize(function() {
+  	$("#collage").css("left", ($(this).width()- $("#collage").width())/2);
+  });
+}
+
+function handleButtonClick() {
+  var searchQuery = $("#input-field").val();
+  if(searchQuery.length > 0 && searchQuery != undefined) {
+    var collageObj = makeRequest(searchQuery);
+    collages.push(collageObj);
+
+    $("body").removeClass("default");
+    $("body").addClass("activated");
+    setTimeout(function(){
+      $("#collage").css("left", ($(this).width()- $("#collage").width())/2);
+    }, 550);
+  }
+}
+
+function makeRequest(queryString) { // Make request to Servelet
+
+  $.get(url + $.param({ query: queryString }))
+  .done(function(data) {
+    return {
+      src: data.src,
+      name: queryString,
+      isActive: false, // if it will show up in the timeline
+    };
+  })
+  .fail(function() {
+    console.log("Request to " + url + $.param({ query: queryString }) + " failed");
+  });
+}
