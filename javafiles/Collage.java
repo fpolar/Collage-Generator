69c71113816dda210424b8880243f4a6afe3f24c
Daniel Mizrahi <dmizrahi@usc.edu>
2018-02-25 17:06:55 -0800

    Merged my stuff in

--- javafiles/Collage.java
+++ javafiles/Collage.java
@@ -1,7 +1,9 @@
 package DataContainers;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,8 +11,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+
 public class Collage extends Picture {
 	private ArrayList<Image> mImages;
+	private static final int NUM_IMAGES = 30;
 	private String mName;
 	public ArrayList<Image> getImages() {
 		return mImages;
@@ -32,36 +37,28 @@ public class Collage extends Picture {
 	//converts the collage to a png, saves it to the server's HD, 
 	//and returns the string with the location
 	public String convertToPng() {
-		URL url;
-		try {
-			url = new URL("https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/66a56988-2537-426f-b25b-149c7a720ac7/07-functions-opt.jpg");
-			InputStream in = new BufferedInputStream(url.openStream());
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			byte[] buf = new byte[1024];
-			int n = 0;
-			while (-1!=(n=in.read(buf)))
-			{
-			   out.write(buf, 0, n);
-			}
-			out.close();
-			in.close();
-			byte[] response = out.toByteArray();
-			FileOutputStream fos = new FileOutputStream("test.jpg");
-			fos.write(response);
-			fos.close();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
 		
+		for (int i = 0; i < NUM_IMAGES; i++) {
+			BufferedImage image = null;
+			try {
+			    URL url = new URL("https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/66a56988-2537-426f-b25b-149c7a720ac7/07-functions-opt.jpg");
+			    image = ImageIO.read(url);
+			   // File file = new File("test.jpg");
+			    //ImageIO.write(image, "png", file);
+			    
+			    
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+			
 		return "";
 	}
 	public static void main(String [] args) {
 		Collage c = new Collage();
 		c.convertToPng();
+		
 	}
 	
 }
